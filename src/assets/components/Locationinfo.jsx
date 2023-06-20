1ca912f2999b99563e67dfcd3f144d3b1ef34df2
millos910 <emontero910@gmail.com>
2023-06-20 10:49:53 -0500

    ya casi

--- src/assets/components/Locationinfo.jsx
+++ src/assets/components/Locationinfo.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-
+import React from 'react'
+import './styles/locationCard.css'
 const Locationinfo = ({ location }) => {
     if (!location) {
         return null; // Si location es null, no renderizar nada
     }
     console.log(location);
 return (
-    <article>
-    <h2>{location.name}</h2>
-    <ul>
-        <li>
-        <span>Type</span>
-        <span>{location.type}</span>
+    <article className='card-location'>
+    <h2 className='locatio-name'>{location.name}</h2>
+    <ul className='location-list'>
+        <li className='location-value'>
+        <span className='location-name-value'>Type</span><br />
+        <span className='location-name-asigment'>{location.type}</span>
+        </li>
+        <li className='location-value'>
+            <span className='location-name-value'>Dimension</span><br />
+            <span className='location-name-asigment'>{location.dimension}</span>
         </li>
         <li>
-            <span>Dimension</span>
-            <span>{location.dimension}</span>
-        </li>
-        <li>
-            <span>Population</span>
-            <span> {location.residents?.length || 0}</span>
+            <span className='location-value'>Population</span><br />
+            <span className='location-name-asigment'> {location.residents?.length || 0}</span>
         </li>
     </ul>
     </article>
